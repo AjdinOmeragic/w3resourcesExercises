9fdfbd83600fc09869c21985246868dd3ded1b0d
Ajdin <ajdin.omeragic.pl@gmail.com>
2024-08-06 23:11:31 +0200

    Fourth Commit

---
+++ csExercises/CustomExercises/3DateTime.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+    	DateTime dateTime = DateTime.Today;
+    	Console.WriteLine("Time: {0}", dateTime);
+    }
+}
