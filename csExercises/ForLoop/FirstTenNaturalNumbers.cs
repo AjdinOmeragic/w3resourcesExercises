35426030add0948866ff69b17e1b5c122301841b
Ajdin <ajdin.omeragic.pl@gmail.com>
2024-08-07 23:48:20 +0200

    First Commit

---
+++ csExercises/ForLoop/FirstTenNaturalNumbers.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        for(int i = 1; i <= 10; i ++)
+        Console.WriteLine ("{0}",i);
+    }
+}
