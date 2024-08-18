36c0d263ee5822a561e8b3529b7fb9a52a7bbdc7
Ajdin <ajdin.omeragic.pl@gmail.com>
2024-08-18 23:45:38 +0200

    First Commit

---
+++ csExercises/Basic/hello.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "!");
+    }
+}
