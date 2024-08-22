e139c6b57a26c7dbaa4d8c4dbd2e93bfab69205f
Ajdin <ajdin.omeragic.pl@gmail.com>
2024-08-22 22:41:50 +0200

    Fourth Commit: Task 70 remove first and last element of string

---
+++ csExercises/Basic/70RemoveCharacter.cs
@@ -0,0 +1,26 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Test strings
+        string[] testStrings = { "PHP", "Python", "JavaScript" };
+
+        foreach (var str in testStrings)
+        {
+            Console.WriteLine($"OG string: {str}");
+            if (str.Length > 2)
+            {
+                string result = str.Substring(1, str.Length - 2);
+                Console.WriteLine($"After operation: {result}");
+            }
+            else
+            {
+                Console.WriteLine("To short man");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
